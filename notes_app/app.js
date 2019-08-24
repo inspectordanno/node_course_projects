3662c82076110f6e0ebcdee2669fab7da1dd469c
Dan Spector <danno2423@gmail.com>
2019-08-24 16:03:02 -0400

    importing node.js core modules

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,13 +1,4 @@
 const fs = require('fs');
 
-for (let i = 0; i < 100; i++) {
-  if (i % 15 === 0) {
-    console.log(i + ' is divisible by 15');
-  } else if (i % 5 === 0) {
-    console.log(i +' is divisible by 5');
-  } else if (i % 3 === 0) {
-    console.log(i + ' is divisible by 3');
-  }
-};
-
 //fs.writeFileSync('notes.txt', 'My name is Dan!');
+fs.appendFileSync('notes.txt',' And I like farts!');
