5827d9ef2f216eee633342120bb3f967c9969c5d
Dan Spector <danno2423@gmail.com>
2019-09-22 17:24:24 -0400

    yargs part ii

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,12 +1,54 @@
-const getNotes = require('./notes.js');
 const chalk = require('chalk')
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-const command = process.argv[2];
-console.log(process.argv)
+//customize yargs version
+yargs.version('1.1.0');
 
-if (command === 'add') {
-  console.log('Adding note');
-} else if (command === 'remove') {
-  console.log('Removing note!');
-}
+//create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => {
+    console.log(`Title:${argv.title}`);
+    console.log(`Body:${argv.body}`);
+  }
+});
+
+//create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => console.log('Removing the note!')
+});
+
+//create list command
+yargs.command({
+  command: 'list',
+  describe: 'Listing the notes',
+  handler: () => console.log('Listing the notes!')
+});
+
+//create read command
+yargs.command({
+  command: 'read',
+  describe: 'Reading the notes',
+  handler: () => console.log('Reading the notes!')
+});
+
+yargs.parse();
+
+
 
