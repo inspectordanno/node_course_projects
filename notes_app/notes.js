abab36428c036e741c5b52d10dab9d8997bb4cce
Dan Spector <danno2423@gmail.com>
2019-10-05 14:09:40 -0400

    adding a note

--- notes_app/notes.js
+++ notes_app/notes.js
@@ -1,5 +1,45 @@
+const fs = require('fs');
+
 const getNotes = () => {
   return 'your notes';
 }
 
-module.exports = getNotes;
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+
+  saveNotes(notes);
+}
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+}
+
+const addNote = function(title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function(note) {
+    return note.title === title;
+  });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log('new note added!');
+  } else {
+    console.log('note title taken');
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+}
