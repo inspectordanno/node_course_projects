2495b2cff794a26222aa2ebc63d55f385c8615a3
Dan Spector <danno2423@gmail.com>
2019-10-26 15:39:05 -0400

    steyling the application: part ii

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,17 +1,27 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 console.log(__dirname);
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
   res.render('index', {
-    title: 'Weather App',
+    title: 'Weather',
     name: 'Danno'
   });
 });
@@ -26,8 +36,21 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help',
+    name: 'Danno',
     message: 'You might need some help, but everything is going to be ok.'
   });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    error: 'help article not found'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    error: '404 not found'
+  })
 });
 
 app.get('/weather', (req, res) => {
@@ -36,6 +59,8 @@ app.get('/weather', (req, res) => {
     location: 'New York City'
   });
 });
+
+
 
 app.listen(3000, () => {
   console.log('server is up on port 3000');
