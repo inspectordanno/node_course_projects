6866f34f861dbf30d5edfee9a0e5171340c0bcc9
Dan Spector <danno2423@gmail.com>
2020-02-27 14:19:38 -0500

    86. Data Validation and Sanitization: Part II

--- task_manager/src/db/mongoose.js
+++ task_manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useCreateIndex: true,
@@ -8,20 +9,72 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('email is invalid');
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('password cannot contain password');
+      }
+    },
   }
 });
 
-const me = new User({
-  name: 'Danno',
-  age: 'poop'
+// const me = new User({
+//   name: '    Mike   ',
+//   email: 'FART@GMAIL.COM ',
+//   password: 'fartsdsdffds'
+// });
+
+// me.save().then(() => {
+//   console.log(me);
+// }).catch((error) => {
+//   console.log('Error!', error);
+// });
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
 });
 
-me.save().then(() => {
-  console.log(me);
+const eggs = new Task({
+  description: '  buy eggs    '
+})
+
+eggs.save().then(() => {
+  console.log(eggs);
 }).catch((error) => {
   console.log('Error!', error);
-})
+});
