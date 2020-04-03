118f5b81fc94db32f6fe9130c0a13dfd34787413
Dan Spector <danno2423@gmail.com>
2020-04-02 20:55:11 -0400

    114. The User/Task Relationship

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -6,18 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('GET requests are disabled');
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send('Site is in maintenance mode');
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -25,3 +13,18 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
+
+const Task = require('./models/task');
+const User = require('./models/user');
+
+const main = async () => {
+  // const task = await Task.findById("5e8681ddc42e3961c47c1cd3");
+  // await task.populate('owner').execPopulate();
+  // console.log(task.owner);
+
+  const user = await User.findById("5e8679f31e53985f220c71d3");
+  await user.populate('tasks').execPopulate();
+  console.log(user.tasks);
+}
+
+main();
