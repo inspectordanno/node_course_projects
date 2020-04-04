83bf20be3409c1916c981b49ba8039721a57c348
Dan Spector <danno2423@gmail.com>
2020-04-03 20:01:37 -0400

    118. Working with Timestamps

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -14,17 +14,17 @@ app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
 
-const Task = require('./models/task');
-const User = require('./models/user');
+// const Task = require('./models/task');
+// const User = require('./models/user');
 
-const main = async () => {
-  // const task = await Task.findById("5e8681ddc42e3961c47c1cd3");
-  // await task.populate('owner').execPopulate();
-  // console.log(task.owner);
+// const main = async () => {
+//   // const task = await Task.findById("5e8681ddc42e3961c47c1cd3");
+//   // await task.populate('owner').execPopulate();
+//   // console.log(task.owner);
 
-  const user = await User.findById("5e8679f31e53985f220c71d3");
-  await user.populate('tasks').execPopulate();
-  console.log(user.tasks);
-}
+//   const user = await User.findById("5e8679f31e53985f220c71d3");
+//   await user.populate('tasks').execPopulate();
+//   console.log(user.tasks);
+// }
 
-main();
+// main();
