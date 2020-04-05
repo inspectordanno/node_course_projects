fc2bd0ebcd43e7c13503e6362b4402875276a0de
Dan Spector <danno2423@gmail.com>
2020-04-04 21:11:53 -0400

    123. Adding Support for File Uploads

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -6,6 +6,14 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const multer = require('multer');
+const upload = multer({
+  dest: 'images'
+});
+app.post('/upload', upload.single('upload'), (req, res) => {
+  res.send();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -13,18 +21,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-//   // const task = await Task.findById("5e8681ddc42e3961c47c1cd3");
-//   // await task.populate('owner').execPopulate();
-//   // console.log(task.owner);
-
-//   const user = await User.findById("5e8679f31e53985f220c71d3");
-//   await user.populate('tasks').execPopulate();
-//   console.log(user.tasks);
-// }
-
-// main();
